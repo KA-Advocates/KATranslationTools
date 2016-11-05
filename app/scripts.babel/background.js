efbb039d5b559ee16f050893579fb54139e0433b
alani <alain.schaefer@gmail.com>
2016-11-05 17:37:40 +0900

    changed double quotes to single quotes

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -2,6 +2,6 @@
 // React when a browser action's icon is clicked.
 chrome.browserAction.onClicked.addListener(function(tab) {
 
-	chrome.tabs.create({url:"https://github.com/KA-Advocates/KATranslationTools"});
+	chrome.tabs.create({url:'https://github.com/KA-Advocates/KATranslationTools'});
 
 });
